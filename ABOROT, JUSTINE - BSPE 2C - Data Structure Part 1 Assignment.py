448a1331b2a7db5170ad3cb90d1fc0067b0c2639
Justine Jude Aborot <202340263@psu.palawan.edu.ph>
2025-04-23 23:48:33 +0800

    Add files via upload
    ICT 1L - Assignment

---
+++ ABOROT, JUSTINE - BSPE 2C - Data Structure Part 1 Assignment.py
@@ -0,0 +1,5 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> list=["Jade","Uvite","Stolzite","Tin","Iron","Nickel","Eakerite","Jadarite","Unakite","Diamond","Earlandite","Dolomite","Annite","Bismuth","Obsidian","Rhenium","Opal","Talc"]
+>>> print(list)
+['Jade', 'Uvite', 'Stolzite', 'Tin', 'Iron', 'Nickel', 'Eakerite', 'Jadarite', 'Unakite', 'Diamond', 'Earlandite', 'Dolomite', 'Annite', 'Bismuth', 'Obsidian', 'Rhenium', 'Opal', 'Talc']
